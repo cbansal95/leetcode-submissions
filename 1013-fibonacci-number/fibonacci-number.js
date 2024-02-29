2328e2d207089b9ab5ac1d1dad8b97076f8ad34b
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-02-29 13:07:38 +0530

    Time: 50 ms (80.96%) | Memory: 48.8 MB (21.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var fib = function(n) {
-    // function fibmem(n, memo = {}){
-    //     if(n in memo) return memo[n]
-    //     if(n == 0) return 0
-    //     if(n == 1) return 1
-    //     if(n==2) return 1
+    function fibmem(n, memo = {}){
+        if(n in memo) return memo[n]
+        if(n == 0) return 0
+        if(n == 1) return 1
+        if(n==2) return 1
 
-    //     memo[n] = fibmem(n-1) + fibmem(n-2)
-    //     return memo[n]
-    // }
-    // return fibmem(n)
-    return n==0?0:n==1?1:fib(n-1)+fib(n-2);
+        memo[n] = fibmem(n-1, memo) + fibmem(n-2, memo)
+        return memo[n]
+    }
+    return fibmem(n)
+    // return n==0?0:n==1?1:fib(n-1)+fib(n-2);
 
 };
