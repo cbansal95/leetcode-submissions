03e67c8aa10c4dd9795651103550980cfa8a4759
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-03-01 19:31:18 +0530

    Time: 134 ms (33.95%) | Memory: 57.2 MB (15.94%) - LeetSync

---
+++ 322-coin-change/coin-change.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    let dp = new Array(amount+1).fill(amount+1)
+    dp[0] = 0
+    for(let i = 1; i <= amount; i++){
+        for(c of coins){
+            if(i - c >= 0){
+                dp[i] = Math.min(dp[i], 1 + dp[i -  c])
+            }
+        }
+    }
+    console.log(dp)
+    if(dp[amount] == amount +1) return -1; else return dp[amount]
+};
