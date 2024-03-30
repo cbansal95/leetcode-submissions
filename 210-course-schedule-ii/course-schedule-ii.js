5f35b10012c467e3f057112848dd2d1ec1284bfa
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-03-30 13:40:14 +0530

    Time: 114 ms (14.58%) | Memory: 58.7 MB (11.31%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    let obj = {}, completed = {}, doing = {}
+    let order = []
+    for(let i = 0; i < numCourses; i++){
+        obj[i] = []
+    }
+    for(let prereq of prerequisites){
+        obj[prereq[0]].push(prereq[1])
+    }
+    console.log(obj)
+    for(let course of Object.keys(obj)){
+        if (dfs(course)){return []}
+    }
+
+    function dfs(course){
+        //check if already completed
+        if(completed[course]) return false
+        //check if can't be completed
+        if(doing[course]) return true
+
+        //mark as doing
+        doing[course] = true
+
+        //dfs on preqs
+        for(let prqs of obj[course]){
+            if(dfs(prqs)) return true
+        }
+
+        //mark completed
+        doing[course] = false
+        completed[course] = true
+
+        //add to array
+        order.push(course)
+        return false
+    }
+    return order
+};
