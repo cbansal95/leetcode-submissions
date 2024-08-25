8571fcc0dbe35eab2682e3c210fbff429b82c9fc
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-08-25 21:51:55 +0530

    Time: 191 ms (5.07%) | Memory: 61.8 MB (5.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.js
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -3,40 +3,42 @@
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-var findOrder = function(numCourses, prerequisites) {
-    let obj = {}, completed = {}, doing = {}
-    let order = []
-    for(let i = 0; i < numCourses; i++){
-        obj[i] = []
+var findOrder = function (numCourses, prerequisites) {
+    let completed = {}, visiting = {}, solution = [], courses = {}
+    //if(!prerequisites || !prerequisites.length) return solution
+    for (let i = 0; i < numCourses; i++) {
+        courses[i] = []
     }
-    for(let prereq of prerequisites){
-        obj[prereq[0]].push(prereq[1])
+    for (const prereq of prerequisites) {
+        console.log(prereq)
+        courses[prereq[0]].push(prereq[1])
     }
-    for(let course of Object.keys(obj)){
-        if (dfs(course)){return []}
+    for (const course of Object.keys(courses)) {
+        if (visiting[course]) return []
+        if (completed[course]) continue
+        if (dfs(course) == false) return []
     }
+    return solution
 
-    function dfs(course){
-        //check if already completed
-        if(completed[course]) return false
-        //check if can't be completed
-        if(doing[course]) return true
-
-        //mark as doing
-        doing[course] = true
-
-        //dfs on preqs
-        for(let prqs of obj[course]){
-            if(dfs(prqs)) return true
+    function dfs(course) {
+        visiting[course] = true
+        let stack = []
+        if(courses[course])
+        for (const p of courses[course]) {
+            if (visiting[p]) return false
+            if (completed[p]) continue
+            stack.push(p)
         }
 
-        //mark completed
-        doing[course] = false
-        completed[course] = true
+        while (stack.length) {
+            let curr = stack.pop()
+            if(completed[curr]) continue
+            let temp = dfs(curr)
+            if (temp == false) return temp
+        }
 
-        //add to array
-        order.push(course)
-        return false
+        solution.push(course)
+        completed[course] = true
+        delete visiting[course]
     }
-    return order
 };
