34893f4ee0ce46aba30b1f344f74777f83680ab1
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-09-03 17:59:54 +0530

    Time: 34 ms (81.98%) | Memory: 16.6 MB (37.00%) - LeetSync

--- 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/sum-of-digits-of-string-after-convert.py
@@ -1,10 +1,8 @@
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         s1 = ""
-        sum = 0
         for c in s:
             s1+=str(ord(c)-96)
-        print(s1)
         for i in range(0,k):
             temp = 0
             for c in s1:
