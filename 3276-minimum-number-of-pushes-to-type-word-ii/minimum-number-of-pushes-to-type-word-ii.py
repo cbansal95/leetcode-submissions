134370d0baecd05b45708bb377fa36827cc10432
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2024-09-05 11:55:10 +0530

    Time: 251 ms (13.04%) | Memory: 17.5 MB (98.68%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,34 +1,3 @@
-def mergeSort(a):
-    if len(a) > 2:
-        mid = len(a)//2
-        return mergeSorted(a[0:mid], a[mid:])
-    if len(a) == 2 and a[0]["freq"] > a[1]["freq"]:
-        a.reverse() 
-    return a
-
-def mergeSorted(a1,a2):
-    out = []
-    c1,c2 = 0,0
-    while c1 < len(a1) and c2 < len(a2):
-        if a1[c1]["freq"] < a2[c2]["freq"]:
-            out.append(a1[c1])
-            c1+=1
-        elif a1[c1]["freq"] > a2[c2]["freq"]:
-            out.append(a2[c2])
-            c2+=1
-        else:
-            out.append(a1[c1])
-            out.append(a2[c2])
-            c1+=1
-            c2+=1
-    while c1 < len(a1):
-        out.append(a1[c1])
-        c1+=1
-    while c2 < len(a2):
-        out.append(a2[c2])
-        c2+=1
-    return out
-
 class Solution:
     def minimumPushes(self, word: str) -> int:
         obj = {}
@@ -44,10 +13,7 @@ class Solution:
             to["char"] = c
             to["freq"] = obj[c]
             temp.append(to)
-
-        # temp = mergeSort(temp)
         temp.sort(key=lambda character: character["freq"], reverse=True)
-        print(temp)
         for i,to in enumerate(temp):
             operations = operations + ((i//8)+1) * to["freq"]
         return operations
