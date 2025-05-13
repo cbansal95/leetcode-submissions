70b0d9240c236627d3199fed95f6f67a4a848ee4
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-05-13 16:56:02 +0530

    Time: 5 ms (16.51%) | Memory: 56.9 MB (20.87%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    const numMap = {}
+    const output = []
+    let i = 0
+    while(i < numbers.length) {
+        if(numMap[target - numbers[i]] !== undefined) {
+            return [numMap[target - numbers[i]] + 1, i + 1]
+        }
+        numMap[numbers[i]] = i
+        i++
+    }
+};
