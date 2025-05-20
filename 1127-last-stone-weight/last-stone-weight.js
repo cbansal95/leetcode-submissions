b41bda27c67177a95dfd55a5406bb7a3fc7e8039
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-05-21 00:12:15 +0530

    Time: 33 ms (5.73%) | Memory: 59.4 MB (5.16%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -1,43 +1,104 @@
+class Heap {
+    constructor(nums) {
+        this.heap = nums
+        this.buildHeap()
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1
+        while (index > 0 && this.heap[index] > this.getParent(index)) {
+            this.swap(index, Math.floor((index-1) / 2))
+            index = Math.floor((index-1) / 2)
+        }
+    }
+    heapifyDown(index) {
+        let largest = index
+        let left = (index * 2) + 1
+        let right = (index * 2) + 2
+        if (left < this.heap.length && this.heap[left] > this.heap[largest]) {
+            largest = left
+        }
+        if (right < this.heap.length && this.heap[right] > this.heap[largest]) {
+            largest = right
+        }
+        if (largest !== index) {
+            this.swap(largest, index)
+            this.heapifyDown(largest)
+        }
+    }
+    swap(index1, index2) {
+        let temp = this.heap[index1]
+        this.heap[index1] = this.heap[index2]
+        this.heap[index2] = temp
+    }
+    hasParent(index) {
+        const parentIndex = Math.floor((index - 1) / 2)
+        if (parentIndex <= 0 || parentIndex > this.heap.length) return false
+        return true
+    }
+    getParent(index) {
+        return this.heap[Math.floor((index - 1) / 2)]
+    }
+    hasLeftChild(index) {
+        const lcIndex = (index * 2) + 1
+        return lcIndex < this.heap.length
+    }
+    getLeftChild(index) {
+        return this.heap[(index * 2) + 1]
+    }
+    hasRightChild(index) {
+        const rcIndex = (index * 2) + 2
+        return rcIndex < this.heap.length
+    }
+    getRightChild(index) {
+        return this.heap[(index * 2) + 2]
+    }
+    add(num) {
+        this.heap.push(num)
+        if(this.heap.length > 1) {
+            this.heapifyUp(this.heap.length - 1)
+        }
+    }
+    remove() {
+        if (this.heap.length == 1) {
+            let temp = this.heap[0]
+            this.heap = []
+            return temp
+        }
+        let temp = this.heap.pop()
+        let temp2 = this.heap[0]
+        this.heap[0] = temp
+        this.heapifyDown(0)
+        return temp2
+    }
+    size() {
+        return this.heap.length
+    }
+    buildHeap() {
+        for (let i = Math.floor(this.heap.length / 2) - 1; i >= 0; i--) {
+            this.heapifyDown(i)
+        }
+    }
+    getAll(){
+        return this.heap
+    }
+}
+
 /**
  * @param {number[]} stones
  * @return {number}
  */
 var lastStoneWeight = function (stones) {
-    function moveL(arr) {
-        let n = arr.length
-
-        for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-            heapify(arr, n, i)
+    let stoneHeap = new Heap(stones)
+    console.log(stoneHeap.getAll())
+    while (stoneHeap.size() > 1) {
+        let stone1 = stoneHeap.remove()
+        let stone2 = stoneHeap.remove()
+        console.log(stoneHeap.getAll(), Math.abs(stone1 - stone2))
+        if (stone1 - stone2 != 0) {
+            stoneHeap.add(Math.abs(stone1 - stone2))
         }
     }
-
-    function heapify(arr, n, i) {
-        let largest = i
-        let l = i * 2 + 1
-        let r = i * 2 + 2
-        if (l < n && arr[l] > arr[largest]) {
-            largest = l
-        }
-
-        if (r < n && arr[r] > arr[largest]) {
-            largest = r
-        }
-        if (largest !== i) {
-            let temp = arr[i]
-            arr[i] = arr[largest]
-            arr[largest] = temp
-            heapify(arr, n, largest)
-        }
-    }
-    while (stones.length > 1) {
-        moveL(stones)
-        let x = stones.shift()
-        moveL(stones)
-        let y = stones.shift()
-        let diff = Math.abs(x - y)
-        if (diff > 0) stones.push(diff)
-    }
-    if (stones.length == 1) return stones[0];
+    if (stoneHeap.size() == 1) return stoneHeap.getAll()[0]
     return 0
 };
-
