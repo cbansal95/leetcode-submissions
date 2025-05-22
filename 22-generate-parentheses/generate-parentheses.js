9268d2f59917c207f91b13a6490da99240ecc578
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-05-22 20:04:38 +0530

    Time: 1 ms (88.12%) | Memory: 52.9 MB (97.93%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = []
+    function backtrack(s='', left = 0, right = 0){
+        if (s.length == 2*n){
+            result.push(s)
+            return
+        }
+        if(left < n) {
+            backtrack(s+'(', left + 1, right)
+        }
+        if(right < left){
+            backtrack(s+')', left, right+1)
+        }
+    }
+    backtrack()
+    return result
+};
