2a43c2fdc59b58ca2c28ed18fb8e174eed019b52
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-05-24 21:15:49 +0530

    Time: 23 ms (14.68%) | Memory: 64 MB (26.91%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -3,38 +3,38 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-var canFinish = function (numCourses, prerequisites) {
+var canFinish = function(numCourses, prerequisites) {
+    let completed = {}
+    let pending = {}
     let courses = {}
-    let visited = {}
-    let visiting = {}
-    for (prereq of prerequisites) {
-        if (courses[prereq[0]] == undefined) {
-            courses[prereq[0]] = [prereq[1]]
+    for (const [c1,c2] of prerequisites) {
+        if (courses[c1]) {
+            courses[c1].push(c2)
         } else {
-            courses[prereq[0]].push(prereq[1])
+            courses[c1] = [c2]
         }
     }
-    for (let course of Object.keys(courses)) {
-        if (dfs(course)) return false
+    const toComplete = Object.keys(courses)
+    for(const course of toComplete){
+        if(dfs(course) == false) return false
     }
-    function dfs(course) {
-        visiting[course] = true
+    function dfs(course){
+        pending[course] = true
+        const prereqs = courses[course]
 
-        let preqs = courses[course]
-        if (!preqs || preqs == undefined || preqs == []) {
-            visited[course] = true
-            visiting[course] = false
-            return false
-
+        if(!prereqs || prereqs == undefined || prereqs == []) {
+            completed[course] = true
+            delete pending[course]
+            return true
         }
-        for (let preq of preqs) {
-            if (visited[preq] && visited[preq] == true) continue;
-            if (visiting[preq] && visiting[preq] == true) return true
-            if (dfs(preq)) return true
+        for(let prereq of prereqs){
+            if(completed[prereq] && completed[prereq] == true) continue
+            if(pending[prereq] && pending[prereq] == true) return false
+            if(dfs(prereq) == false) return false
         }
-        visited[course] = true
-        delete visiting[course]
-        return false
+        completed[course] = true
+        delete pending[course]
+        return true
     }
     return true
 };
