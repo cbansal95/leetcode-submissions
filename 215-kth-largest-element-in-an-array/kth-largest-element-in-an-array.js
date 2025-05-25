f6abc336210757a12b30c558b05caa0263053d93
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-05-25 19:54:36 +0530

    Time: 102 ms (7.94%) | Memory: 67.5 MB (48.82%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -3,41 +3,12 @@
  * @param {number} k
  * @return {number}
  */
-var findKthLargest = function (nums, k) {
-    function sort(arr) {
-        let n = arr.length
-
-        for (let i = Math.floor(n / 2); i >= 0; i--) {
-            heapify(arr, n, i)
-        }
-    for (var i = n - 1; i > 0; i--) {
-        // Move current root to end
-        var temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-
-        // call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-    }
-
-    function heapify(arr, n, i) {
-        let largest = i
-        let l = (2 * i) + 1
-        let r = (2 * i) + 2
-
-        if (l < n && arr[l] > arr[largest]) largest = l
-        if (r < n && arr[r] > arr[largest]) largest = r
-
-        if (largest != i) {
-            let temp = arr[i]
-            arr[i] = arr[largest]
-            arr[largest] = temp
-
-            heapify(arr, n, largest)
-        }
-    }
-    sort(nums)
-    console.log(nums)
-    return nums[nums.length - (k)]
+var findKthLargest = function(nums, k) {
+    sortedNums = nums.sort((a,b) => {
+        if(a > b) return -1
+        if(b > a) return 1
+        if(b==1) return 0
+    })
+    console.log(sortedNums)
+    return sortedNums[k-1]
 };
