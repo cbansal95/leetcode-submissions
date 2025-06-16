d591ef6525b87494eb094f1486e67a82d07e0ea4
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-06-16 21:51:34 +0530

    Time: 1 ms (89.71%) | Memory: 60.7 MB (92.57%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.js
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.js
@@ -14,22 +14,21 @@ var findMode = function(root) {
     let currentVal = null, currentCount = 0, maxCount = 0, modes = []
 
     function inOrderTraversal(node) {
-        if (!node) return;
+        if(!node) return
 
-        inOrderTraversal(node.left);
+        inOrderTraversal(node.left)
 
-        currentCount = (node.val === currentVal) ? currentCount + 1 : 1;
-        if (currentCount === maxCount) {
-            modes.push(node.val);
-        } else if (currentCount > maxCount) {
-            maxCount = currentCount;
-            modes = [node.val];
+        if (node.val == currentVal) {
+            currentCount++
+        } else {currentCount = 1}
+        if (currentCount == maxCount) {modes.push(node.val)} else if (currentCount > maxCount) {
+            maxCount = currentCount
+            modes = [node.val]
         }
-        currentVal = node.val;
+        currentVal = node.val
 
-        inOrderTraversal(node.right);
+        inOrderTraversal(node.right)
     }
-
     inOrderTraversal(root)
     return modes
 };
