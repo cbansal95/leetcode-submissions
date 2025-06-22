08c0ee74ad42d18842591b5c8cc52a56774339dd
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-06-22 15:43:23 +0530

    Time: 3 ms (69.01%) | Memory: 59.7 MB (66.83%) - LeetSync

--- 39-combination-sum/combination-sum.js
+++ 39-combination-sum/combination-sum.js
@@ -5,20 +5,18 @@
  */
 var combinationSum = function(candidates, target) {
     let out = []
-
     function rec(i, curr, total){
-        //define target
         if(total == target){
             out.push([...curr])
             return
         }
-        if(total > target || i >= candidates.length) return
-        //form next iterations
+        if(i >= candidates.length || total > target) return
         curr.push(candidates[i])
         rec(i, curr, total + candidates[i])
         curr.pop()
         rec(i+1, curr, total)
     }
-    rec(0, [], 0)
+
+    rec(0,[],0)
     return out
 };
