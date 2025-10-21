d836d12a28ddb81824a5e67a9cb8e8c44f50248c
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-10-21 19:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 59.7 MB (20.57%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.js
+++ 328-odd-even-linked-list/odd-even-linked-list.js
@@ -10,17 +10,36 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    if(!head || !head.next) return head
-    let odd = head
-    let even = head.next
-    let evenHead = even
-
-    while(even && even.next){
-        odd.next = even.next
-        odd = odd.next
-        even.next = odd.next
-        even = even.next
+    if(!head) return head
+    let flag = false
+    let l1t = new ListNode()
+    let l2t = new ListNode()
+    let temp = head, l1 = l1t, l2 = l2t
+    while(temp){
+        if (flag){
+            l1.next = new ListNode(temp.val)
+            l1 = l1.next
+        } else {
+            l2.next = new ListNode(temp.val)
+            l2 = l2.next
+        }
+        flag = !flag
+        temp = temp.next
     }
-    odd.next = evenHead
-    return head
+    l1t = l1t.next
+    l2t = l2t.next
+    let out = new ListNode()
+    let curr = out
+    while(l2t){
+        curr.next = new ListNode(l2t.val)
+        l2t = l2t.next
+        curr = curr.next
+    }
+    while(l1t){
+        curr.next = new ListNode(l1t.val)
+        l1t = l1t.next
+        curr = curr.next
+    }
+    
+    return out.next
 };
