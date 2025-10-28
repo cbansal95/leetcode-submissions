1614914875f5e31b649f03dcc8f78c818821a3c1
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2025-10-28 19:06:01 +0530

    Time: 33 ms (93.52%) | Memory: 76.1 MB (49.22%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.js
@@ -1,67 +1,39 @@
-class TreeNode{
-    constructor(){
-        this.children = {}
-        this.eow = false
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+    insert(word){
+        let curr = this.root
+        for (const c of word){
+            if (curr.children[c] == undefined) {
+                curr.children[c] = new TrieNode()
+                curr = curr.children[c]
+            } else curr = curr.children[c]
+        }
+        curr.isEndOfWord = true
+    }
+    startsWith(word){
+        let curr = this.root
+        for (const c of word){
+            if (curr.children[c] == undefined) {
+                return false
+            } else curr = curr.children[c]
+        }
+        return true
+    }
+    search(word){
+                let curr = this.root
+        for (const c of word){
+            if (curr.children[c] == undefined) {
+                return false
+            } else curr = curr.children[c]
+        }
+        return curr.isEndOfWord
     }
 }
-
-var Trie = function() {
-    this.root = new TreeNode()
-};
-
-/** 
- * @param {string} word
- * @return {void}
- */
-Trie.prototype.insert = function(word) {
-    let curr = this.root
-
-    for(const c of word){
-        if(curr.children[c] == undefined){
-            const newChild = new TreeNode
-            curr.children[c] = newChild
-            curr = newChild
-        } else {
-            curr = curr.children[c]
-        }
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
     }
-    curr.eow = true
-};
-
-/** 
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function(word) {
-    let curr = this.root
-
-    for(const c of word){
-        if(curr.children[c] == undefined) return false; else {
-            curr = curr.children[c]
-        }
-    }
-    return curr.eow
-};
-
-/** 
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
-    let curr = this.root
-
-    for(const c of prefix){
-        if(curr.children[c] == undefined) return false; else {
-            curr = curr.children[c]
-        }
-    }
-    return true
-};
-
-/** 
- * Your Trie object will be instantiated and called as such:
- * var obj = new Trie()
- * obj.insert(word)
- * var param_2 = obj.search(word)
- * var param_3 = obj.startsWith(prefix)
- */
+}
