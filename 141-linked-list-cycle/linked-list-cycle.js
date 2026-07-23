83636682d081fa30ec441f004af370bfd623a6d9
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2026-07-23 13:34:03 +0530

    Time: 51 ms (74.28%) | Memory: 59 MB (63.88%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -13,9 +13,10 @@
 var hasCycle = function(head) {
     let fast = head
     while(fast && fast.next){
-        head = head.next
+        
         fast = fast.next.next
-        if(fast == head) return true
+        head = head.next
+        if (fast == head) return true
     }
     return false
 };
