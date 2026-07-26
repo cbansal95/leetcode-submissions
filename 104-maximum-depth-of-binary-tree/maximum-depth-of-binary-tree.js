5661977eb91894e1fecfa9b3a967d246aac8d16c
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2026-07-26 16:53:03 +0530

    Time: 1 ms (51.12%) | Memory: 60.9 MB (28.45%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -12,5 +12,5 @@
  */
 var maxDepth = function(root) {
     if(!root) return 0
-    return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1)
-};
+    return Math.max((maxDepth(root.left)+1), (maxDepth(root.right)+1))
+}; 
