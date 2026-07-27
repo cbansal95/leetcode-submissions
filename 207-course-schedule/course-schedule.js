34e436681c40b247e8f11779ec88fdc2942b58a6
cbansal95 <52032726+cbansal95@users.noreply.github.com>
2026-07-27 22:08:39 +0530

    Time: 20 ms (16.85%) | Memory: 64.6 MB (11.94%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -4,37 +4,31 @@
  * @return {boolean}
  */
 var canFinish = function(numCourses, prerequisites) {
-    let completed = {}
-    let pending = {}
-    let courses = {}
-    for (const [c1,c2] of prerequisites) {
-        if (courses[c1]) {
-            courses[c1].push(c2)
-        } else {
-            courses[c1] = [c2]
-        }
+    const completed = {}, pending = {}, courses = {}
+    for(const [a,b] of prerequisites){
+        if(courses[a]){
+            courses[a].push(b)
+        } else courses[a] = [b]
     }
-    const toComplete = Object.keys(courses)
-    for(const course of toComplete){
+
+    for(const course of Object.keys(courses)){
         if(dfs(course) == false) return false
     }
-    function dfs(course){
-        pending[course] = true
-        const prereqs = courses[course]
+    return true
 
-        if(!prereqs || prereqs == undefined || prereqs == []) {
-            completed[course] = true
-            delete pending[course]
-            return true
+    function dfs(course) {
+        if (pending[course]) return false
+        if (completed[course]) return true
+
+        pending[course] = true
+        const prereqs = courses[course] || []
+
+        for (const p of prereqs) {
+            if (!dfs(p)) return false
         }
-        for(let prereq of prereqs){
-            if(completed[prereq] && completed[prereq] == true) continue
-            if(pending[prereq] && pending[prereq] == true) return false
-            if(dfs(prereq) == false) return false
-        }
+
         completed[course] = true
         delete pending[course]
         return true
     }
-    return true
 };
